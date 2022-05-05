676dbdea8fc5a88475949d5cf2bc518b235a4201
Bertolen <metroidbruno@hotmail.com>
2022-05-05 12:09:19 +0200

    Première version fonctionnelle

--- src/main/java/com/eulerhermes/dri/fileaid/Panels/StructuredDataPanel.java
+++ src/main/java/com/eulerhermes/dri/fileaid/Panels/StructuredDataPanel.java
@@ -1,5 +1,6 @@
 package com.eulerhermes.dri.fileaid.Panels;
 
+import java.awt.*;
 import java.util.List;
 
 import javax.swing.BoxLayout;
@@ -8,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 
 public class StructuredDataPanel extends JPanel {
 
@@ -30,7 +33,7 @@ public class StructuredDataPanel extends JPanel {
 	
 	/**
 	 * Receptionne les données à afficher sous forme d'un tableau à deux dimentions
-	 * @param structuredData
+	 * @param structuredData liste à 2D des données à afficher
 	 */
 	public void setStructuredData(List<List<String>> structuredData) {
 		
@@ -39,18 +42,38 @@ public class StructuredDataPanel extends JPanel {
 		panneauDeroulant.validate();
 		
 		// initialisation de la table
-		DefaultTableModel tableModel = new DefaultTableModel(structuredData.get(0).toArray(), 0);
-		structuredData.remove(0);
-		
+		DefaultTableModel tableModel = new DefaultTableModel(0, structuredData.get(0).size());
+
 		// alimente la nouvelle table
 		for (List<String> ligne : structuredData) {
 			tableModel.addRow(ligne.toArray());
-			System.out.println("Ajoute la ligne : " + ligne.toString());
 		}
 
 		// ajoute la table au panneau
 		JTable table = new JTable(tableModel);
 		panneauDeroulant.add(table);
+
+		// Redimentionne le tableau pour le rendre visible
+	}
+
+	public void resizeTable(JTable table) {
+
+		// Calcule la taille opti pour les colonnes
+		TableColumnModel columnModel = table.getColumnModel();
+		for (int i = 0 ; i < table.getColumnCount() ; i++){ // boucle sur les colonnes
+			int width = 15; // largeur minimale
+			for (int j = 0 ; j < table.getRowCount() ; j++){ // boucle sur les lignes
+				TableCellRenderer renderer = table.getCellRenderer(j,i);
+				Component comp = table.prepareRenderer(renderer,j,i);
+				width = Math.max(comp.getPreferredSize().width + 1 , width);
+			}
+			if(width > 300) width = 300;// largeur maximale
+			columnModel.getColumn(i).setPreferredWidth(width);
+		}
+
+		// applique la taille opti
+		table.setSize(table.getPreferredSize());
+
 	}
 
 }
